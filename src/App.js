11a49436d1757d1b49c33ccea774bfe511bb8394
tylerccaceres <tyler.caceres@gmail.com>
2019-09-26 20:30:23 -0400

    useState hooks included

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [searchText, setSearchText] = useState("");
+  const [searchYear, setSearchYear] = useState("");
+
   return (
-    <div>
-      <input required placeholder="search field"></input>
+    <>
+      <input
+        required
+        placeholder="search field"
+        values={searchText}
+        onChange={e => setSearchText(e.target.value)}
+      ></input>
       <select name="type">
+        <option selected value="">
+          -- type --
+        </option>
         <option value="movie">movie</option>
         <option value="series">series</option>
         <option value="episode">episode</option>
       </select>
-      <input placeholder="year"></input>
+      <input placeholder="year" values={searchYear} onChange={e => setSearchYear(e.target.value)}></input>
       <button>search</button>
-    </div>
+    </>
   );
 };
 
