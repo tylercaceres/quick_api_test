ec6d3f074fc8433dea772e8c0cfa39496ef2528f
tylerccaceres <tyler.caceres@gmail.com>
2019-09-27 10:25:55 -0400

    search functionality created

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-//omd api key --> d93bd1eb
-
 const App = () => {
   const [searchText, setSearchText] = useState("");
   const [searchYear, setSearchYear] = useState("");
+  const [data, setData] = useState([]);
 
   const onSearch = async e => {
     e.preventDefault();
     try {
-      const data = await axios.get("http://www.omdbapi.com", {
+      const newData = await axios.get("http://www.omdbapi.com", {
         params: {
           apikey: "d93bd1eb",
           s: searchText,
-          y: searchYear,
-          page: 2
+          y: searchYear
         }
       });
-      console.log(data);
+      console.log(newData.data.Search);
+      setData(newData.data.Search);
     } catch (err) {
       console.log("error :", err);
     }
   };
 
   return (
-    <>
+    <div>
       <form onSubmit={onSearch}>
         <input
           required
@@ -42,7 +41,20 @@ const App = () => {
         <input placeholder="year" values={searchYear} onChange={e => setSearchYear(e.target.value)}></input>
         <button>search</button>
       </form>
-    </>
+
+      {data.length > 0 &&
+        data.map(movie => {
+          return (
+            <div key={movie.imdbID}>
+              <h1>{movie.Title}</h1>
+              <h1>{movie.Year}</h1>
+              <h1>{movie.imdbID}</h1>
+              <h1>{movie.Type}</h1>
+              <img alt={movie.Title} src={movie.Poster}></img>
+            </div>
+          );
+        })}
+    </div>
   );
 };
 
